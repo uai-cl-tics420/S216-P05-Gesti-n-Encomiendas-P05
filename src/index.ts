0ee08d21c15fc43eed756f0362f5a2e86844173a
Ignacio Millar <imillar@alumnos.uai.cl>
2026-06-23 09:37:33 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET || "incharge-
 const google = new Google(
   process.env.GOOGLE_CLIENT_ID!,
   process.env.GOOGLE_CLIENT_SECRET!,
-  "http://localhost:3000/api/auth/google/callback"
+  "https://incharge-production.up.railway.app/api/auth/google/callback"
 );
 
 const server = serve({
